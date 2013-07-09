9140df6893e4e6089bb5848783d76627a21d8f71
Mingming Yin <mingming@codeaurora.org>
2013-07-09 14:58:53 -0700

    audio: do not invalidate stream used by voip
    During the process of invalidation of voip streams,
    actual driver is not closed so when it tries to open again
    getoutput fails as only one instance of voip can be created.
    so do not invalidate the tracks used by voip driver.
    CRs-Fixed: 494093
    
    Change-Id: I42a962991ce1d586d87f4aecba19c0e5dde6b9dd

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -1906,6 +1906,7 @@ audio_io_handle_t AudioFlinger::openOutput(audio_module_handle_t module,
         }
         if (thread != NULL) {
             mPlaybackThreads.add(id, thread);
+            thread->mOutputFlags = flags;
         }
 
         if (pSamplingRate != NULL) {
@@ -2281,7 +2282,11 @@ status_t AudioFlinger::setStreamOutput(audio_stream_type_t stream, audio_io_hand
 
     for (size_t i = 0; i < mPlaybackThreads.size(); i++) {
         PlaybackThread *thread = mPlaybackThreads.valueAt(i).get();
-        thread->invalidateTracks(stream);
+#ifdef QCOM_HARDWARE
+        // Do not invalidate voip stream which uses directoutput thread
+        if(!(thread->type() == ThreadBase::DIRECT && (thread->mOutputFlags & AUDIO_OUTPUT_FLAG_VOIP_RX)))
+#endif
+            thread->invalidateTracks(stream);
     }
 
     return NO_ERROR;
